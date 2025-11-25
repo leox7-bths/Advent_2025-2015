34203665121c1140fe125fa4fe9e4f1d3947bc15
Home <m3e660@gmail.com>
2025-11-24 22:00:54 -0500

    Advent 2015 Day 3 Part 1 & 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Main {
@@ -15,38 +13,64 @@ public class Main {
 
 
     public static int getPartOneNumber(ArrayList<String> lines) {
-        int total = 0;
-        int past = 0;
-        int current = 0;
-        for (int i = 0; i < lines.size(); i++) {
-            if (i == 0) {
-                current = Integer.parseInt(lines.get(i));
-                past = Integer.parseInt(lines.get(i));
-            } else {
-                current = Integer.parseInt(lines.get(i));
-                if (current > past) {
-                    total++;
+        Set<String> visited = new HashSet<>();
+
+        int x = 0;
+        int y = 0;
+        visited.add(x + "," + y);
+
+        for (String line : lines) {
+            for (char move : line.toCharArray()) {
+                switch (move) {
+                    case '^': y++; break;
+                    case 'v': y--; break;
+                    case '<': x--; break;
+                    case '>': x++; break;
                 }
-                past = Integer.parseInt(lines.get(i));
+                visited.add(x + "," + y);
             }
         }
-        return total;
+
+        return visited.size();
     }
 
     public static int getPartTwoNumber(ArrayList<String> lines) {
-        int total = 0;
-        int first = 0;
-        int second = 0;
-        int third = 0;
-        ArrayList<String> biggerList = new ArrayList<>();
-        for (int i = 0; i < lines.size()-2; i++) {
-            first = Integer.parseInt(lines.get(i));
-            second = Integer.parseInt(lines.get(i+1));
-            third = Integer.parseInt(lines.get(i+2));
-            total = first + second + third;
-            biggerList.add("" + total);
+        Set<String> visited = new HashSet<>();
+//        Set<String> visited2 = new HashSet<>();
+
+        int x = 0;
+        int y = 0;
+        int x2 = 0;
+        int y2 = 0;
+        visited.add(x + "," + y);
+
+
+        for (String line : lines) {
+            for (int i = 0; i < line.length(); i++) {
+                if (i % 2 == 0) {
+                    char move = line.charAt(i);
+                    switch (move) {
+                        case '^': y++; break;
+                        case 'v': y--; break;
+                        case '<': x--; break;
+                        case '>': x++; break;
+                    }
+                    visited.add(x + "," + y);
+                } else {
+                    char move = line.charAt(i);
+                    switch (move) {
+                        case '^': y2++; break;
+                        case 'v': y2--; break;
+                        case '<': x2--; break;
+                        case '>': x2++; break;
+                    }
+                    visited.add(x2 + "," + y2);
+                }
+            }
         }
-        return getPartOneNumber(biggerList);
+
+
+        return visited.size();
     }
 
     public static ArrayList<String> getFileData(String fileName) {
