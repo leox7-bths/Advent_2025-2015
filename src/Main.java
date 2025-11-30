720838769f42e25fa382782ac205fdb5f6b7ce53
Home <m3e660@gmail.com>
2025-11-30 10:48:08 -0500

    Advent 2015 Day 6 Part 1 & 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 
@@ -10,160 +9,138 @@ public class Main {
         ArrayList<String> lines = getFileData("src/data");
         System.out.println("Part one answer: " + getPartOneNumber(lines));
         System.out.println("Part two answer: " + getPartTwoNumber(lines));
-
-
     }
 
 
-    public static int getPartOneNumber(ArrayList<String> lines) {
-
-
-        HashMap<String, Integer> map = new HashMap<>();
-
-
-        map.put("a", 0);
-        map.put("b", 0);
-        map.put("c", 0);
-        map.put("d", 0);
-
-
-        int temp = 0;
-        int jnz = 0;
-        int i = 0;
-
-
-        while (i < lines.size()) {
-            String line = lines.get(i);
-            String[] parts = line.split(" ");
-            String op = parts[0];
-
-            switch (op) {
-                case "cpy": {
-                    String x = parts[1];
-                    String y = parts[2];
-                    int value;
-
-                    if (map.containsKey(x)) {
-                        value = map.get(x);
-                    } else {
-                        value = Integer.parseInt(x);
+    public static int getPartOneNumber(ArrayList<String> line) {
+        boolean[][] grid = new boolean[1000][1000];
+        for (String lines : line) {
+            String[] parts = lines.split(" ");
+            if (parts.length == 4) {
+                String first = parts[1];
+                String second = parts[3];
+                String[] cord1 = first.split(",");
+                String cord11 = cord1[0];
+                String cord12 = cord1[1];
+                String[] cord2 = second.split(",");
+                String cord21 = cord2[0];
+                String cord22 = cord2[1];
+                for (int x = Integer.parseInt(cord11); x <= Integer.parseInt(cord21); x++) {
+                    for (int y = Integer.parseInt(cord12); y <= Integer.parseInt(cord22); y++) {
+                        if (grid[x][y]) {
+//                            System.out.println(x + "," + y);
+                            grid[x][y] = false;
+                        } else {
+//                            System.out.println(x + "," + y);
+                            grid[x][y] = true;
+                        }
                     }
-
-                    map.put(y, value);
-                    i++;
-                    break;
                 }
+            } else {
+                String onOff = parts[1];
+                String first = parts[2];
+                String second = parts[4];
+                String[] cord1 = first.split(",");
+                String cord11 = cord1[0];
+                String cord12 = cord1[1];
+                String[] cord2 = second.split(",");
+                String cord21 = cord2[0];
+                String cord22 = cord2[1];
+//                System.out.println(onOff);
+//                System.out.println(first);
+//                System.out.println(second);
+//                System.out.println(cord11);
+//                System.out.println(cord12);
+//                System.out.println(cord21);
+//                System.out.println(cord22);
 
-                case "inc": {
-                    String r = parts[1];
-                    map.put(r, map.get(r) + 1);
-                    i++;
-                    break;
-                }
-
-                case "dec": {
-                    String r = parts[1];
-                    map.put(r, map.get(r) - 1);
-                    i++;
-                    break;
-                }
-
-                case "jnz": {
-                    String x = parts[1];
-                    String y = parts[2];
-
-                    int value = map.containsKey(x) ? map.get(x) : Integer.parseInt(x);
-                    int offset = map.containsKey(y) ? map.get(y) : Integer.parseInt(y);
-
-                    if (value != 0) {
-                        i += offset;
-                    } else {
-                        i++;
+                for (int x = Integer.parseInt(cord11); x <= Integer.parseInt(cord21); x++) {
+                    for (int y = Integer.parseInt(cord12); y <= Integer.parseInt(cord22); y++) {
+                        if (onOff.equals("on")) {
+//                            System.out.println(x + "," + y);
+                            grid[x][y] = true;
+                        } else {
+//                            System.out.println(x + "," + y);
+                            grid[x][y] = false;
+                        }
                     }
-                    break;
                 }
-
-                default:
-                    i++;
             }
         }
-        return map.get("a");
+
+        int count = 0;
+        for (int a = 0; a < 1000; a++) {
+            for (int b = 0; b < 1000; b++) {
+                if (grid[a][b] == true) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 
 
-    public static int getPartTwoNumber(ArrayList<String> lines) {
-
-
-        HashMap<String, Integer> map = new HashMap<>();
-
-
-        map.put("a", 0);
-        map.put("b", 0);
-        map.put("c", 1);
-        map.put("d", 0);
-
-
-        int temp = 0;
-        int jnz = 0;
-        int i = 0;
-
-
-        while (i < lines.size()) {
-            String line = lines.get(i);
-            String[] parts = line.split(" ");
-            String op = parts[0];
-
-            switch (op) {
-                case "cpy": {
-                    String x = parts[1];
-                    String y = parts[2];
-                    int value;
-
-                    if (map.containsKey(x)) {
-                        value = map.get(x);
-                    } else {
-                        value = Integer.parseInt(x);
+    public static int getPartTwoNumber(ArrayList<String> line) {
+        int[][] grid = new int[1000][1000];
+        for (String lines : line) {
+            String[] parts = lines.split(" ");
+            if (parts.length == 4) {
+                String first = parts[1];
+                String second = parts[3];
+                String[] cord1 = first.split(",");
+                String cord11 = cord1[0];
+                String cord12 = cord1[1];
+                String[] cord2 = second.split(",");
+                String cord21 = cord2[0];
+                String cord22 = cord2[1];
+                for (int x = Integer.parseInt(cord11); x <= Integer.parseInt(cord21); x++) {
+                    for (int y = Integer.parseInt(cord12); y <= Integer.parseInt(cord22); y++) {
+                        grid[x][y] += 2;
                     }
-
-                    map.put(y, value);
-                    i++;
-                    break;
                 }
+            } else {
+                String onOff = parts[1];
+                String first = parts[2];
+                String second = parts[4];
+                String[] cord1 = first.split(",");
+                String cord11 = cord1[0];
+                String cord12 = cord1[1];
+                String[] cord2 = second.split(",");
+                String cord21 = cord2[0];
+                String cord22 = cord2[1];
+//                System.out.println(onOff);
+//                System.out.println(first);
+//                System.out.println(second);
+//                System.out.println(cord11);
+//                System.out.println(cord12);
+//                System.out.println(cord21);
+//                System.out.println(cord22);
 
-                case "inc": {
-                    String r = parts[1];
-                    map.put(r, map.get(r) + 1);
-                    i++;
-                    break;
-                }
-
-                case "dec": {
-                    String r = parts[1];
-                    map.put(r, map.get(r) - 1);
-                    i++;
-                    break;
-                }
-
-                case "jnz": {
-                    String x = parts[1];
-                    String y = parts[2];
-
-                    int value = map.containsKey(x) ? map.get(x) : Integer.parseInt(x);
-                    int offset = map.containsKey(y) ? map.get(y) : Integer.parseInt(y);
-
-                    if (value != 0) {
-                        i += offset;
-                    } else {
-                        i++;
+                for (int x = Integer.parseInt(cord11); x <= Integer.parseInt(cord21); x++) {
+                    for (int y = Integer.parseInt(cord12); y <= Integer.parseInt(cord22); y++) {
+                        if (onOff.equals("on")) {
+//                            System.out.println(x + "," + y);
+                            grid[x][y]++;
+                        } else {
+//                            System.out.println(x + "," + y);
+                            if (grid[x][y] != 0) {
+                                grid[x][y]--;
+                            }
+                        }
                     }
-                    break;
                 }
-
-                default:
-                    i++;
             }
         }
-        return map.get("a");
+
+        int count = 0;
+        for (int a = 0; a < 1000; a++) {
+            for (int b = 0; b < 1000; b++) {
+                count += grid[a][b];
+            }
+        }
+
+        return count;
     }
 
 
